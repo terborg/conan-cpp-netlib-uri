13e8b367f490136938249cf7e81b6f2b2bc067f7
Rutger ter Borg <rutger.terborg@artibit.com>
2018-07-24 09:18:31 +0200

    update

--- conanfile.py
+++ conanfile.py
@@ -42,7 +42,7 @@ conan_basic_setup()''')
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy("lib/libnetwork-uri.a", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["network-uri"]
